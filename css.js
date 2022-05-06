eae6ee689abcb585e41aac664d61d7aad6f3e81d
LittleAkira <cinzia.ariotti.10@gmail.com>
2022-05-06 08:56:34 +0200

    update

--- css.js
+++ css.js
@@ -1,11 +1,14 @@
 function writeDocument (array){
-    const container = document.getElementById('todo-container')
+
+    const container = document.getElementById('todo-container');
+
     for (const element of array) {
+
       const div = document.createElement('div');
-      div.classList.add('todo-object')
+      div.classList.add('todo-object');
 
       const title = document.createElement('h1');
-      const textTitle = document.createTextNode(element.title)
+      const textTitle = document.createTextNode(element.title);
       title.appendChild(textTitle);
       div.appendChild(title);
 
@@ -17,16 +20,30 @@ function writeDocument (array){
         //   tagDiv.appendChild(tagPar);
         //   div.appendChild(tagDiv);
         const tagDiv = document.createElement('p');
-        tagDiv.classList.add("tag")
+        tagDiv.classList.add("tag");
         const tagText = document.createTextNode(tag);
-        tagDiv.appendChild(tagText)
-        div.appendChild(tagDiv)
+        tagDiv.appendChild(tagText);
+        div.appendChild(tagDiv);
       }
 
-      const date =document.createElement('p')
-      const textDate = document.createTextNode(element.creationDate.toDateString());
-      date.appendChild(textDate)
-      div.appendChild(date)
+      const date = document.createElement('div');
+
+      const create =document.createElement('p');
+      const textCreate = document.createTextNode('Creato: ' + element.creationDate.toDateString());
+      create.classList.add("date")
+      create.appendChild(textCreate);
+      date.appendChild(create)
+      div.appendChild(date);
+
+      const expire =document.createElement('p');
+      const textExpire = document.createTextNode('Scade il: ' + element.creationDate.toDateString());
+      expire.classList.add("exp-date")
+      expire.appendChild(textExpire);
+      date.appendChild(expire)
+      div.appendChild(date);
+
+
+      
 
 
       if (element.priority === ToDo.PRIORITY.low){
